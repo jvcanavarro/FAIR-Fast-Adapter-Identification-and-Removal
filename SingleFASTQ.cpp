b98dbc41c5986c032e4ba39758cfc771e26e4e4d
João Canavarro <jvcanavarro@gmail.com>
2019-01-25 11:15:21 -0300

    Added First Methods.

--- SingleFASTQ.cpp
+++ SingleFASTQ.cpp
@@ -1,4 +1,57 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
+class singleFASTQ
+{
+	string id, seq, pholder, qual;
+public:
+	void setIdentifier(string id);
+	string getIdentifier();
+	void setSequence(string seq);
+	string getSequence();
+	void setPlaceHolder(string pholder);
+	string getPlaceHolder();
+	void setQuality(string qual);
+	string getQuality();
+	vector <int> convertQualityToInteger();
+	void trim(string adapter, int minQuality, int minSequenceLength);
+};
+
+
+void singleFASTQ::setIdentifier(string id)
+{
+	id = id;
+}
+
+string singleFASTQ::getIdentifier()
+{
+	return id;
+}
+void singleFASTQ::setSequence(string seq)
+{
+	seq = seq;
+}
+
+string singleFASTQ::getSequence()
+{
+	return seq;
+}
+void singleFASTQ::setPlaceHolder(string pholder)
+{
+	pholder = pholder;
+}
+
+string singleFASTQ::getPlaceHolder()
+{
+	return pholder;
+}
+void singleFASTQ::setQuality(string qual)
+{
+	qual = qual;
+}
+
+string singleFASTQ::getQuality()
+{
+	return qual;
+}
--- main.cpp
+++ main.cpp
@@ -1,13 +1,6 @@
-#include <iostream>
-#include <locale.h>
-#include "SingleFASTQ.h"
-using namespace std;
+#include "SingleFASTQ.cpp"
 
 int main (){
 	setlocale(LC_ALL, "Portuguese");
-	printf("Hello World\n");
-	T teste;
-	teste.test=  3;
-	printf("%d\n", teste.test);
 	return 0;
-}
+} 
