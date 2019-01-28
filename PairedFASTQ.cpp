4f9115610c107cbdee2eed366a37bcc6c0d7846d
João Canavarro <jvcanavarro@gmail.com>
2019-01-28 10:29:22 -0300

    Removed test files, added methods on PairedFASTQ.cpp

---
+++ PairedFASTQ.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include "SingleFASTQ.cpp"
+using namespace std;
+
+class PairedFASTQ
+{
+	pair <SingleFASTQ, SingleFASTQ> pair; 
+public:
+	void setPair(SingleFASTQ foward, SingleFASTQ reverse);
+	pair <SingleFASTQ, SingleFASTQ> getPair();
+	void trim(string adapter1, string adapter2, int minQuality, int minSequenceLength);
+};
+
+void PairedFASTQ::setPair(SingleFASTQ foward, SingleFASTQ reverse)
+{
+	pair = make_pair(foward, reverse);
+}
+pair <SingleFASTQ, SingleFASTQ> PairedFASTQ::getPair(){
+	return pair;
+}
