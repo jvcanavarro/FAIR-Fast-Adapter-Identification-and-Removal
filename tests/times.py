2c3a4eaa040fd5d0e7d794bff3d4757a33e5a624
Canavarro <jvcanavarro@gmail.com>
2019-05-22 12:57:26 -0300

    Improving Plot

--- tests/times.py
+++ tests/times.py
@@ -1,7 +1,18 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-columns = ['Name', 'plen8', 'plen16', 'plen32', 'plen64']
+
+columns = ['plen8', 'plen16', 'plen32', 'plen64']
 
 data = pd.read_csv('all8-64.csv', names=columns, sep='\t', lineterminator='\n')
-print(data.head())
+# print(data.iloc[:2])
+
+data['means'] = data.mean(axis=1)
+data['std'] = data.std(axis=1)
+print(data.head(10))
+top5 = data.sort_values('means').head(5)
+print(top5)
+print(top5.iloc[0, 0])
+
 
